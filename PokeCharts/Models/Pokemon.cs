5cf1f6d7c81e3b38a7872c1fe7fe5ec866303543
maurxsantoz <maurx18@gmail.com>
2022-12-22 08:39:29 +0100

    Add Pokemon Model

---
+++ PokeCharts/Models/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿namespace PokeCharts.Models
+{
+    public class Pokemon
+    {
+        private int id;
+        private string name;
+        private float height;
+        private float weight;
+        private string sprite;
+
+        public Pokemon(int id, string name, float height, float weight, string sprite)
+        {
+            this.id = id;
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+            this.sprite = sprite;
+        }
+
+        public int Id { get => id; }
+        public string Name { get => name; }
+        public float Height { get => height; }
+        public float Weight { get => weight; }
+        public string Sprite { get => sprite; }
+    }
+}

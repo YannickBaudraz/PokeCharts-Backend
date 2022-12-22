86f1275f68e97ba349469e43e70e3cfc166f7c81
maurxsantoz <maurx18@gmail.com>
2022-12-22 09:05:06 +0100

    Add Interface for pokemonDao

---
+++ PokeCharts/Daos/IPokemonDao.cs
@@ -0,0 +1,11 @@
+﻿using PokeCharts.Models;
+namespace PokeCharts.Daos
+{
+    public interface IPokemonDao
+    {
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        List<Pokemon> GetPokemons();
+
+    }
+}

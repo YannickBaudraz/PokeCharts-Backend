4fb12744e41fdb18b7f8b046d51e2659f3f44483
maurxsantoz <maurx18@gmail.com>
2022-12-22 09:26:57 +0100

    Adds pokemonDao and its interface

--- PokeCharts/Daos/IPokemonDao.cs
+++ PokeCharts/Daos/IPokemonDao.cs
@@ -1,11 +1,10 @@
 ﻿using PokeCharts.Models;
-namespace PokeCharts.Daos
-{
-    public interface IPokemonDao
-    {
-        Pokemon GetPokemon(int id);
-        Pokemon GetPokemon(string name);
-        List<Pokemon> GetPokemons();
 
-    }
+namespace PokeCharts.Daos;
+
+public interface IPokemonDao
+{
+    Pokemon GetPokemon(int id);
+    Pokemon GetPokemon(string name);
+    List<Pokemon> GetPokemons();
 }
---
+++ PokeCharts/Daos/PokemonDao.cs
@@ -0,0 +1,19 @@
+﻿using PokeCharts.Models;
+
+namespace PokeCharts.Daos;
+
+public class PokemonDao : IPokemonDao
+{
+    public Pokemon GetPokemon(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Pokemon GetPokemon(string name)
+    {
+        throw new NotImplementedException();
+    }
+    public List<Pokemon> GetPokemons()
+    {
+        throw new NotImplementedException();
+    }
+}

e0effd05189b8ee6649adc6aa2f6fd288908d394
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-01-12 13:06:37 +0100

    Add test for complex query

--- PokeCharts.UnitTests/Query/GraphQlQueryBuilderTest.cs
+++ PokeCharts.UnitTests/Query/GraphQlQueryBuilderTest.cs
@@ -10,10 +10,9 @@ public class GraphQlQueryBuilderTest
     public void Build_SimpleQuery_ReturnsCorrectQuery()
     {
         // Given
-        // Get resource file
         string expectedQuery = ResourceTestHelper.GetGraphQlQuery("SimpleQuery.txt");
         GraphQlQueryBuilder queryBuilder = new GraphQlQueryBuilder("allPokemon")
-            .Field("pokemon_v2_pokemon", builder => builder
+            .Field("pokemon_v2_pokemon", b => b
                 .Field("id")
                 .Field("name")
             );
@@ -32,20 +31,68 @@ public class GraphQlQueryBuilderTest
         string expectedQuery = ResourceTestHelper.GetGraphQlQuery("QueryWithSimpleCondition.txt");
         const string type = "poison";
         GraphQlQueryBuilder queryBuilder = new GraphQlQueryBuilder("allOfSpecificType")
-            .FieldWithArguments("pokemon_v2_type", typeArgBuilder => typeArgBuilder
-                .Argument("where", whereBuilder => whereBuilder
-                    .Argument("name", nameBuilder => nameBuilder
+            .FieldWithArguments("pokemon_v2_type", b => b
+                .Argument("where", b => b
+                    .Argument("name", b => b
                         .ArgumentCondition("_eq", type)))
                 .EndArguments()
                 .Field("name")
-                .Field("pokemon_v2_pokemontypes", typeBuilder => typeBuilder
-                    .Field("pokemon_v2_pokemon", pokemonBuilder => pokemonBuilder
+                .Field("pokemon_v2_pokemontypes", b => b
+                    .Field("pokemon_v2_pokemon", b => b
                         .Field("name")
                         .Field("id"))));
 
         // When
         string query = queryBuilder.Build();
 
+        // Then
+        Assert.That(query, Is.EqualTo(expectedQuery));
+    }
+
+    [Test]
+    public void Build_ComplexQuery_ReturnsCorrectQuery()
+    {
+        // Given
+        string expectedQuery = ResourceTestHelper.GetGraphQlQuery("ComplexQuery.txt");
+        GraphQlQueryBuilder queryBuilder = new GraphQlQueryBuilder("weakestPokemonAbleToBeatFireRedAlone")
+            .FieldWithArguments("pokemon_v2_pokemon", b => b
+                .Argument("where", b => b
+                    .Argument("_and", b => b
+                        .Argument("pokemon_v2_pokemonspecy", b => b
+                            .Argument("pokemon_v2_generation", b => b
+                                .Argument("name", b => b
+                                    .ArgumentCondition("_eq", "generation-i"))))
+                        .Argument("pokemon_v2_pokemonmoves", b => b
+                            .Argument("pokemon_v2_move", b => b
+                                .Argument("name", b => b
+                                    .ArgumentCondition("_eq", "strength"))))
+                        .Argument("_and", b => b
+                            .Argument("pokemon_v2_pokemonmoves", b => b
+                                .Argument("pokemon_v2_move", b => b
+                                    .Argument("name", b => b
+                                        .ArgumentCondition("_eq", "cut"))))
+                            .Argument("_and", b => b
+                                .Argument("pokemon_v2_pokemonmoves", b => b
+                                    .Argument("pokemon_v2_move", b => b
+                                        .Argument("name", b => b
+                                            .ArgumentCondition("_eq", "surf"))))))))
+                .Argument("order_by", b => b
+                    .ArgumentConditionEnum("base_experience", "asc"))
+                .ArgumentCondition("limit", 1)
+                .EndArguments()
+                .Field("pokemon_v2_pokemonspecy", b => b
+                    .FieldWithArguments("pokemon_v2_pokemonspeciesnames", b => b
+                        .Argument("where", b => b
+                            .Argument("pokemon_v2_language", b => b
+                                .Argument("name", b => b
+                                    .ArgumentCondition("_eq", "en"))))
+                        .EndArguments()
+                        .Field("name")))
+            );
+        
+        // When
+        string query = queryBuilder.Build();
+        
         // Then
         Assert.That(query, Is.EqualTo(expectedQuery));
     }

9bca6d9f3ea11cf3a14d1e3772bb4ec376587ec2
maurxsantoz <maurx18@gmail.com>
2023-03-01 09:01:07 +0100

    fixes some version differences

--- PokeCharts/Models/Pokemon.cs
+++ PokeCharts/Models/Pokemon.cs
@@ -8,11 +8,11 @@ public record Pokemon
     public string Name { get; }
     public float Height { get; }
     public float Weight  {get; }
-    public string Sprite { get; }
+    public PokemonSprites Sprites { get; }
     public Stats? Stats { get; } 
     public Type[]? Types { get; }
 
-    public Pokemon (int id, string name, float height, float weight, string sprite, Stats stats, Type[] types)
+    public Pokemon (int id, string name, float height, float weight, PokemonSprites sprites, Stats stats, Type[] types)
     {
         if (types == null || types.Length == 0 || types.Length > 2 ){
             throw new ArgumentException("A pokemon can only have one or two types.");
@@ -21,7 +21,7 @@ public record Pokemon
         this.Name = name;
         this.Height = height;
         this.Weight = weight;
-        this.Sprite = sprite;
+        this.Sprites = sprites;
         this.Stats = stats;
         this.Types = types;
     }

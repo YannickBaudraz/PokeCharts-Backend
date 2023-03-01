1529eb9fef7d07e3a6a63fd725b6776a7fa44a54
Yannick Clifford <yannickbaudrazdev@gmail.com>
2023-03-01 09:59:54 +0100

    Reformat code

--- PokeCharts/Daos/PokemonDao.cs
+++ PokeCharts/Daos/PokemonDao.cs
@@ -1,92 +1,95 @@
-﻿﻿﻿using PokeCharts.GraphQl;
-using PokeCharts.Models;
+﻿using Newtonsoft.Json.Linq;
 using PokeCharts.Exceptions;
+using PokeCharts.GraphQl;
+using PokeCharts.Models;
+
 namespace PokeCharts.Daos;
 
 public class PokemonDao : IPokemonDao
 {
-    private GraphQlClient _client;
-    private IConfiguration _configuration;
-    private QueryConverter _queryConverter;
+    private readonly GraphQlClient _client;
+    private readonly QueryConverter _queryConverter;
 
     public PokemonDao(IConfiguration configuration)
     {
         _client = new GraphQlClient(configuration);
-        _configuration = configuration;
         _queryConverter = new QueryConverter(configuration);
     }
-    
+
     public Pokemon Get(int id)
     {
-        return SendQuery(ConditionalQuery("id",id.ToString()))[0];
+        return SendQuery(ConditionalQuery("id", id.ToString()))[0];
     }
+
     public Pokemon Get(string name)
     {
-        return SendQuery(ConditionalQuery("name",name))[0];
+        return SendQuery(ConditionalQuery("name", name))[0];
     }
+
     public List<Pokemon> Get()
     {
         string query = new GraphQlQueryBuilder("")
-          .Field("Pokemons: pokemon_v2_pokemon", b => b
-            .Field("Id: id")
-            .Field("Name: name")
-            .Field("Height: height")
-            .Field("Weight: weight")
-            .Field("Types: pokemon_v2_pokemontypes", b => b
-              .Field("Type: pokemon_v2_type", b => b
+            .Field("Pokemons: pokemon_v2_pokemon", b => b
                 .Field("Id: id")
                 .Field("Name: name")
-              )
-            )
-            .Field("Stats: pokemon_v2_pokemonstats", b => b
-              .Field("Stat: pokemon_v2_stat", b => b
-                .Field("Id: id")
-                .Field("Name: name")
-              )
-              .Field("BaseStat: base_stat")
-            )
-          ).Build();
+                .Field("Height: height")
+                .Field("Weight: weight")
+                .Field("Types: pokemon_v2_pokemontypes", b => b
+                    .Field("Type: pokemon_v2_type", b => b
+                        .Field("Id: id")
+                        .Field("Name: name")
+                    )
+                )
+                .Field("Stats: pokemon_v2_pokemonstats", b => b
+                    .Field("Stat: pokemon_v2_stat", b => b
+                        .Field("Id: id")
+                        .Field("Name: name")
+                    )
+                    .Field("BaseStat: base_stat")
+                )
+            ).Build();
+
         return SendQuery(query);
     }
-    
+
     private List<Pokemon> SendQuery(string query)
     {
-        var result = _client.Execute(query).Result;
+        JObject result = _client.Execute(query).Result;
         List<Pokemon> output = _queryConverter.ToPokemons(result);
+
         if (output.Count > 0)
         {
             return output;
-        }            
-        throw new PokemonNotFoundException(1);
-    }
-    
+        }
 
-    private string ConditionalQuery(string field,string value)
+        throw new PokemonNotFoundException(new ModelReference(1));
+    }
+
+    private string ConditionalQuery(string field, string value)
     {
-       return new GraphQlQueryBuilder("")
-          .FieldWithArguments("Pokemons: pokemon_v2_pokemon", b => b
-            .Argument("where", b => b
-                .Argument(field, b => b
-                    .ArgumentCondition("_eq",value)))
-            .EndArguments()
-            .Field("Id: id")
-            .Field("Name: name")
-            .Field("Height: height")
-            .Field("Weight: weight")
-            .Field("Types: pokemon_v2_pokemontypes", b => b
-              .Field("Type: pokemon_v2_type", b => b
+        return new GraphQlQueryBuilder("")
+            .FieldWithArguments("Pokemons: pokemon_v2_pokemon", b => b
+                .Argument("where", b => b
+                    .Argument(field, b => b
+                        .ArgumentCondition("_eq", value)))
+                .EndArguments()
                 .Field("Id: id")
                 .Field("Name: name")
-              )
-            )
-            .Field("Stats: pokemon_v2_pokemonstats", b => b
-              .Field("Stat: pokemon_v2_stat", b => b
-                .Field("Id: id")
-                .Field("Name: name")
-              )
-              .Field("BaseStat: base_stat")
-            )
-          ).Build();
+                .Field("Height: height")
+                .Field("Weight: weight")
+                .Field("Types: pokemon_v2_pokemontypes", b => b
+                    .Field("Type: pokemon_v2_type", b => b
+                        .Field("Id: id")
+                        .Field("Name: name")
+                    )
+                )
+                .Field("Stats: pokemon_v2_pokemonstats", b => b
+                    .Field("Stat: pokemon_v2_stat", b => b
+                        .Field("Id: id")
+                        .Field("Name: name")
+                    )
+                    .Field("BaseStat: base_stat")
+                )
+            ).Build();
     }
-
 }

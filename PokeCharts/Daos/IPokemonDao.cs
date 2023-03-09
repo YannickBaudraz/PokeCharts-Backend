643081eda2865297775fe261f065e01c75174353
Nomeos <mathieu.rabot01@gmail.com>
2023-03-09 09:35:35 +0100

    Add the getFiltered method in the DAO pokemon interface

--- PokeCharts/Daos/IPokemonDao.cs
+++ PokeCharts/Daos/IPokemonDao.cs
@@ -8,4 +8,6 @@ public interface IPokemonDao
     Pokemon Get(string name);
     List<Pokemon> Get();
     List<string> GetNames();
+
+    List<Pokemon> GetFiltered(string types, string stat, string conditions, int? conditionValue);
 }
